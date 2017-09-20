d05300fa6fe5abe566efc3a1c21318a56164ae82
ryan <ryanmarchock@gmail.com>
2017-09-20 20:08:19 +0100

    reworked grid

--- src/js/components/grid.js
+++ src/js/components/grid.js
@@ -1,138 +1,145 @@
-import Elements from './elements';
+
+export const GridSetup = () => {
+  let array = [];
+  let maxGridColumns = 0;
+  let maxGridRows = 0;
+  let store = null;
+  let row = 0;
+  let column = 0;
+
+  return {
+
+    setup(s) {
+      store = s;
+
+      this
+        .reset()
+        .calNumberOfColumns()
+        .calMaxGridRows()
+        .createRowsAndCols();
+    },
+
+    reset() {
+      array = [];
+      row = 0;
+      column = 0;
+      return this;
+    },
+
+    next(index) {
+      if (column < (store.maxGridColumns - 1)) {
+        column += 1;
+
+      } else {
+        column = 0;
+        row += 1;
 
 
-class Grid {
-
-  /* Reset Grid with new array
-   *
-   * @return: {array}
-   * */
-  static reset() {
-    return [];
-  }
-
-  /* Calculate the maximum rows for the grid
-  *
-  * @param: {object} - store : { {number} - tilesLength, {number} - maxGridColumns}
-  *
-  * @return: number
-  * */
-  static calMaxGridRows({tilesLength, maxGridColumns}) {
-      return Math.round(tilesLength / maxGridColumns);
-  }
-
-  /* Create row and columns for grid
-   *
-   * @param: {object} - store : {{array} - grid {number} - maxGridRows, {number} - maxGridColumns}
-   *
-   * @return: array - grid
-   * */
-  static createRowsAndCols({grid, maxGridRows, maxGridColumns}) {
-    for (let row = 0; row < maxGridRows; row += 1) {
-      // if grid row does not have an array then create an array to represent columns
-      if (!grid[row]) {
-        grid[row] = Grid.createColumns(maxGridColumns);
-      }
-    }
-    return grid;
-  }
-
-  /* Create columns array
-   *
-   * @param: {number}
-   *
-   * @return: array
-   * */
-  static createColumns(columns) {
-    return new Array(columns).fill(0);
-  }
-
-  /* At this grid point is there a space available to place a tile there
-   *
-   * @param: {object} - store: {{array} - grid}
-   * @param: {number} - row
-   * @param: {number} - column
-   *
-   * @return: {number} - returns 1 or 0
-   * */
-  static spaceAvailable({grid}, row, column) {
-    return (arguments.length > 2) ? grid[row][column] : grid[row];
-  }
-
-  static hasSpace(store, tc, row, column) {
-    const { grid, maxGridColumns, tiles } = store;
-    const { width, height } = tiles[tc];
-    let spaceAvailable = 0;
-
-    for (let i = 0; i < height; i += 1) {
-      for (let ii = 0; ii < width; ii += 1) {
-
-        if ((column + ii) < maxGridColumns) {
-          // Add another row to fit tile
-          if (grid.length <= (row + i)) {
-            grid[grid.length] = Grid.createColumns(maxGridColumns);
+        if (index < (store.tilesLength - 1)) {
+          if (!this.isRowAvailable(row)) {
+            this.addNewRow();
           }
-
-          // does grid have space for tile ( 0 is yes and 1 in no)
-          if (grid[row + i][column + ii]) spaceAvailable += 1;
-        } else {
-          spaceAvailable += 1;
         }
       }
-    }
-    return !spaceAvailable;
-  }
+    },
 
-  static update(store, tc, row, column) {
-    const { grid, tiles } = store;
-    const { width, height } = tiles[tc];
+    currentPoint() {
+      return { row, column };
+    },
 
-    for (let i = 0; i < height; i += 1) {
-      for (let ii = 0; ii < width; ii += 1) {
-        grid[row + i][column + ii] = 1;
-      }
-    }
-    return grid;
-  }
+    calNumberOfColumns() {
+      const { tile } = store.settings.breakpoints[store.breakpoints.index];
+      maxGridColumns = Math.floor(store.container.width / tile.width);
+      return this;
+    },
 
-  static newRow({grid, maxGridColumns}) {
-    grid[grid.length] = Grid.createColumns(maxGridColumns);
-    return grid;
-  }
+    calMaxGridRows() {
+      maxGridRows = Math.round(store.tilesLength / maxGridColumns);
+      return this;
+    },
 
-  //store.grid, store.tile.padding, store.settings, store.maxGridColumns
-  static removeEmptyRows(store) {
-    const { showGutter, modifyTileHeight } = store.settings;
-    let d = [];
-
-
-    // search rows for empty columns
-    for (let i = 0; i < store.grid.length; i += 1) {
-        let c = 0;
-        for (let ii = 0; ii < store.grid[i].length; ii += 1) {
-          if (!store.grid[i][ii]) c += 1;
+    createRowsAndCols() {
+      for (let row = 0; row < maxGridRows; row += 1) {
+        // if grid row does not have an array then
+        // create an array to represent columns
+        if (!array[row]) {
+          array[row] = this.createColumns(maxGridColumns);
         }
+      }
+      return this;
+    },
 
-        // if the entire row is false then add row number to be deleted
-        if (c === store.maxGridColumns) d.push(i);
+    getMaxColumns() {
+      return maxGridColumns;
+    },
 
+    getMaxRows() {
+      return maxGridRows;
+    },
+
+    createColumns(columns) {
+      return new Array(columns).fill(0);
+    },
+
+    isSpaceAvailable() {
+      return array[row][column];
+    },
+
+    isRowAvailable() {
+      return array[row];
+    },
+
+    update(tc) {
+      const { width, height } = store.tiles[tc];
+
+      for (let i = 0; i < height; i += 1) {
+        for (let ii = 0; ii < width; ii += 1) {
+          array[row + i][column + ii] = 1;
+        }
+      }
+    },
+
+    newRowRequired() {
+      if (array.length === row) {
+        this.addNewRow();
+      }
+    },
+
+    hasSpace(tc) {
+      const { tiles } = store;
+      const { width, height } = tiles[tc];
+      let spaceAvailable = 0;
+
+      for (let i = 0; i < height; i += 1) {
+        for (let ii = 0; ii < width; ii += 1) {
+
+          if ((column + ii) < maxGridColumns) {
+            // Add another row to fit tile
+            if (array.length <= (row + i)) {
+              array[array.length] = this.createColumns(maxGridColumns);
+            }
+
+            // does grid have space for tile ( 0 is yes and 1 in no)
+            if (array[row + i][column + ii]) spaceAvailable += 1;
+          } else {
+            spaceAvailable += 1;
+          }
+        }
+      }
+      return !spaceAvailable;
+    },
+
+
+    addNewRow() {
+      array[array.length] = this.createColumns(maxGridColumns);
+    },
+
+    removeEmptyRows() {
+      const newArray = array.filter((col) => {
+        return col.find((boolean => boolean));
+      });
+      maxGridRows = newArray.length;
+      array = [ ...newArray ];
     }
-    // reverse array to start the delete loop with the highest number
-    d.reverse();
-
-    for (let i = 0; i < d.length; i += 1) {
-      store.grid.splice(d[i], 1);
-    }
-
-    let height = showGutter
-        ? (store.grid.length * modifyTileHeight)
-        : ((store.grid.length * modifyTileHeight) - store.tile.padding);
-
-
-    Elements.updateContainerHeight(store, height);
-
-    return store.grid;
   }
-}
-
-export default Grid;
+};
